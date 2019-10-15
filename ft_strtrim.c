dd8a14f5bfdc0d3419736d1b0a44f14f67397839
Adrien THEUS <adtheus@e1r1p2.42.fr>
2019-10-16 01:23:28 +0200

    rectifiation de strtrim; refaire substr

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,15 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adtheus <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: adtheus <adtheus@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/12 13:31:14 by adtheus           #+#    #+#             */
-/*   Updated: 2019/10/12 13:37:33 by adtheus          ###   ########.fr       */
+/*   Updated: 2019/10/16 00:44:12 by adtheus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
 #include <stdlib.h>
+#include "libft.h"
 
 static int	is_set(char const char_s1, char const *set)
 {
@@ -27,19 +27,18 @@ static int	is_set(char const char_s1, char const *set)
 char		*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
-	size_t	index_pre;
-	size_t	index_suf;
+	int		index_pre;
+	int		index_suf;
 	char	*str;
 
 	index_pre = 0;
-	index_suf = 0;
-	while (s1[index_suf])
-		index_suf++;
+	index_suf = ft_strlen(s1);
 	while (is_set(s1[index_pre], set))
 		index_pre++;
-	while (is_set(s1[index_suf - 1], set))
+	while (index_suf && is_set(s1[index_suf - 1], set))
 		index_suf--;
-	if (!(str = (char*)malloc(sizeof(*str) * (index_suf - index_pre))))
+	i = index_suf - index_pre > 0 ? index_suf - index_pre : 0;
+	if (!(str = (char*)malloc(sizeof(*str) * (i + 1))))
 		return (NULL);
 	i = 0;
 	while (index_pre < index_suf)
@@ -48,5 +47,19 @@ char		*ft_strtrim(char const *s1, char const *set)
 		i++;
 		index_pre++;
 	}
+	str[i] = '\0';
 	return (str);
 }
+
+/*
+**int main()
+**{
+**	char *s;
+**	char *str = "               ";
+**
+**	s = ft_strtrim(str, " ");
+**	printf("%s\n", s);
+**	free(s);
+**	return(0);
+**}
+*/
--- ft_substr.c
+++ ft_substr.c
@@ -6,12 +6,13 @@
 /*   By: adtheus <adtheus@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 14:41:19 by adtheus           #+#    #+#             */
-/*   Updated: 2019/10/09 15:39:35 by adtheus          ###   ########.fr       */
+/*   Updated: 2019/10/16 01:20:45 by adtheus          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stddef.h>
+#include "libft.h"
+#include <unistd.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -19,14 +20,31 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	n;
 
 	n = 0;
-	if ((str = (char*)malloc(sizeof(*str) * len + 1)) != NULL)
+	if (start < ft_strlen(s))
+	{
+		if (!(str = (char*)malloc(sizeof(*str) * len + 1)))
+			return (NULL);
 		while (n < len && s[n])
 		{
 			str[n] = s[start + n];
 			n++;
 		}
+	}
 	else
-		return (NULL);
+		return ("");
 	str[n] = '\0';
 	return (str);
 }
+
+
+int main()
+{
+	char* a;
+	char* str = "hellofsfgsfgsg";
+
+	a = ft_substr(str, 200, 10);
+	write(1, a, 1);
+	free(a);
+	return (0);
+}
+
